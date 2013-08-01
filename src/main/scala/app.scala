f836dc49258e6b0e36f9586a874f88739e764753
Devon Stewart <blast@hardchee.se>
2013-08-01 19:53:36 +0900

    Refactoring to an entirely javax.mail based solution

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -1,33 +1,17 @@
 package se.hardchee.MailConverter
 
-import javax.mail.Session
-import java.util.Properties
-import javax.mail.internet.MimeMessage
 import java.io.ByteArrayInputStream
+import java.io.File
 
-import scala.util.parsing.combinator.RegexParsers
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
 
-object parser extends RegexParsers {
-    val hostname = "mail.en2go.com"
-
-    val number = rep( "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" )
-    val character = ".".r
-
-    val ipv4 = number ~ "." ~ number ~ "." ~ number ~ "." ~ number
-
-    val headerReceived = "from" ~> hostname ~ ( "(" ~> ( ipv4 | "[unix socket]" ) <~ ")" ) ~ "(.|\\s)*$".r ^^ {
-        case hostname ~ connection ~ rest => hostname
-    }
-
-    val versionIdentifier = rep(character)
-
-    def parseReceived(input: String) = {
-        parseAll(headerReceived, input) match {
-            case Success(hostname, _) => Some(hostname)
-            case failure: NoSuccess => scala.sys.error(failure.msg)
-        }
-    }
-}
+import java.util.Properties
+import javax.mail.Session
+import javax.mail.internet.MimeMessage
+import javax.mail.Message
+import javax.mail.internet.MimeMultipart
+import javax.mail.internet.MimeBodyPart
 
 object app {
     def readRaw(path: String): Option[String] = {
@@ -47,21 +31,11 @@ object app {
         message
     }
 
-    def generateFrom(message: MimeMessage) = {
-        "foo"
-    }
-
-    def prependFrom(raw: String, message: MimeMessage) = {
-        generateFrom(message) + "\r\n" + raw
-    }
 
     def main(args: Array[String]) {
         val target :: files = args.toList
-        println(files)
-        println(target)
-        val output = new FileOutputStream(new File(target))
-        val messages = files.flatMap { readRaw }.map { content => prependFrom(content, readMessage(content)) }
-        output.write(messages.mkString("\r\n\r\n").getBytes)
+        val messages = files.flatMap { readRaw }.map { readMessage }
+        println(messages.mkString)
     }
 }
 
