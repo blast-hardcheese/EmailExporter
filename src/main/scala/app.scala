154171f3044c7988498635f68f7f57511d6f50f4
Devon Stewart <blast@hardchee.se>
2013-08-01 16:24:03 +0900

    Adding a basic email regex parser

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -5,6 +5,30 @@ import java.util.Properties
 import javax.mail.internet.MimeMessage
 import java.io.ByteArrayInputStream
 
+import scala.util.parsing.combinator.RegexParsers
+
+object parser extends RegexParsers {
+    val hostname = "mail.en2go.com"
+
+    val number = rep( "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" )
+    val character = ".".r
+
+    val ipv4 = number ~ "." ~ number ~ "." ~ number ~ "." ~ number
+
+    val headerReceived = "from" ~> hostname ~ ( "(" ~> ( ipv4 | "[unix socket]" ) <~ ")" ) ~ "(.|\\s)*$".r ^^ {
+        case hostname ~ connection ~ rest => hostname
+    }
+
+    val versionIdentifier = rep(character)
+
+    def parseReceived(input: String) = {
+        parseAll(headerReceived, input) match {
+            case Success(hostname, _) => Some(hostname)
+            case failure: NoSuccess => scala.sys.error(failure.msg)
+        }
+    }
+}
+
 object app {
     def readRaw(path: String): String = {
         val content = io.Source.fromFile(new java.io.File(path)).mkString
